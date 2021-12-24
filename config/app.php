6336fb31baeaacbbe0f7aee216bed69f313000fa
Florian Martin <56591109+NeoFeear@users.noreply.github.com>
2021-12-24 17:19:13 +0100

    Last modif

--- config/app.php
+++ config/app.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    'APP_ENV'=> 'prod',
-];
+   'APP_ENV'=> 'prod', 
+   'SQL_USER' => 'root',
+   'SQL_HOST' => 'localhost',
+   'SQL_PASS' => '',
+   'SQL_DTB'=>'to-the-moon', 
+]; 
