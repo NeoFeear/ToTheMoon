275ff327e9d3bc158ac25447fc7832dcfde74043
Allan <allan.breuil@hotmail.fr>
2021-12-26 15:58:01 +0100

    color picker

--- public/assets/js/play_room.js
+++ public/assets/js/play_room.js
@@ -12,91 +12,76 @@ let colorsTable = [
     'DeepSkyBLue'  
 ];
 
-let colorsPicked = [
-    'green',
-    'red',
-    'blue',
-    'yellow',
-    'Tomato',
-    'cyan',
-    'Plum',
-    'Indigo',
-    'LightPink',
-    'Khaki',
-    'DeepSkyBLue'  
-];
+let colorsPicked = [];
 
+//generate les inputs colors
+generateInputColors();
 
-for(let i = 0; i < 6; i++){
+function generateInputColors(){
 
-    let selectColorInput = document.getElementById('colorPicker'+i);
-
-    
-    
-    selectColorInput.addEventListener('change', () => {
-        updateColors('colorPicker'+i);
-        
-    })
-
-    for(let i = 0; i < colorsTable.length; i++){
-
-        let addOption = document.createElement('option');
-
-        addOption.value = colorsTable[i];
-        addOption.innerHTML = colorsTable[i].toUpperCase();
-        addOption.style.backgroundColor = colorsTable[i];
-        
-        selectColorInput.appendChild(addOption);
-        selectColorInput.style.backgroundColor =selectColorInput.value; 
-
-    }
-    
-}
-
-
-function updateColors(selected){
-    let si = document.getElementById(selected);
-
-    //On parcours le tableau de couleurs
-    for( var i = 0; i < colorsTable.length; i++){ 
-        
-        //Si la couleur est présente, on la retire du tableau
-        if (colorsTable[i] == si.value) { 
-            si.style.backgroundColor = si.value;
-
-            colorsTable.splice(i, 1); 
-            i--; 
-
-        }    
-
-    }
-    console.log(colorsTable);
-
-    //On retire les childs de tous les selects qui ont comme valeur la valeur du select
     for(let i = 0; i < 6; i++){
 
-        let selectColorInput = document.getElementById('colorPicker'+i);
-        let childs = Array.from(selectColorInput.children);
+    let selectColorInput = document.getElementById('colorPicker'+i);
+        selectColorInput.dataset.color = "";
 
-        if(selectColorInput.id !== selected){
+    selectColorInput.addEventListener('change', () => {
+        setColorInput(selectColorInput);
+        updateColorInputs(selectColorInput);
+        // console.log(colorsPicked);
 
-            for(let j = 0; j < childs.length; j++){
+    })
 
-                if(childs[j].value == si.value){
-                    childs[j].remove();
-                };
-         
-            }
-        }
+        for(let i = 0; i < colorsTable.length; i++){
+
+            let addOption = document.createElement('option');
+
+            addOption.value = colorsTable[i];
+            addOption.style.backgroundColor = colorsTable[i];
             
-    }   
+            selectColorInput.appendChild(addOption);
+            selectColorInput.style.backgroundColor =selectColorInput.value; 
 
+        }
+
+    }
 }
 
 
-function compareArray(Array1, Arary2){
-    let missing = Array1.filter(e=>!Arary2.includes(e));
-    console.log(missing);
+function setColorInput(selected){
+
+    if(colorsPicked.includes(selected.dataset.color)){
+        const index = colorsPicked.indexOf(selected.dataset.color);
+        colorsPicked.splice(index,1);
+    }
+
+    selected.style.backgroundColor = selected.value
+    selected.dataset.color = selected.value;
+    colorsPicked.push(selected.value);
+
+    console.log(colorsPicked);
+}
+
+function updateColorInputs(selected){
+ 
+    for(let i = 0; i < 6; i++){
+
+        let si = document.getElementById('colorPicker'+i);
+        let childs = Array.from(si.children);
+
+        for(let j = 0; j < childs.length; j++){
+
+            if(colorsPicked.includes(childs[j].value)){
+                childs[j].disabled = true;
+                childs[j].hidden = true;
+            }else{
+                childs[j].disabled = false;
+                childs[j].hidden = false;
+            }
+        
+        }
+        
+    }
+
 }
 
 
