e566bcd2deaecc09762f81a980a52921c968a8f7
Allan <allan.breuil@hotmail.fr>
2022-01-03 15:52:03 +0100

    add js

--- src/App/Controller/Quiz/QuizController.php
+++ src/App/Controller/Quiz/QuizController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
+use Ratchet\ConnectionInterface;
 use Ratchet\Server\IoServer;
 use App\Quiz;
 
@@ -24,40 +25,50 @@ class QuizController extends AbstractController
   public function __invoke(): string
   { 
   
-    //SERIALIZER CONFIG
-    $encoders = [new JsonEncoder()];
-    $normalizers = [new ObjectNormalizer()];
-
-    $serializer = new Serializer($normalizers, $encoders);
-
-    $users = new Users();
-    $sessionUser = $_SESSION['logged'];
-
-    $users = $this->getRepository('users')->findAll();
-
-    //SERIALIZE USER OBJECT EN JSON
-    $jsonSessionUser = $serializer->serialize($sessionUser, 'json');
-
-    
-    
-        
-
-
+    $this->currentUser = $_SESSION['logged'];
 
     // echo '<pre>';print_r($session_user); die;
-    
-        // Faire un curl pour recup l'id de la room
-        
-        // Envoyer un mail avec l'url 
-        // ttm.io/game?roomId=4242
+    // Faire un curl pour recup l'id de la room
+    // Envoyer un mail avec l'url 
+    // ttm.io/game?roomId=4242
+    // sur /game tu recup ton GET roomId
+    // Dans le JS : Socket.io qui contacte le node : 127.0.0.1:8080/joinRoom/4242
 
-        // sur /game tu recup ton GET roomId
-        // Dans le JS : Socket.io qui contacte le node : 127.0.0.1:8080/joinRoom/4242
+    return $this->render('quiz/quiz.html.twig', [
+      
+    ]);
 
-      return $this->render('quiz/quiz.html.twig', [
-        'users' => $users,
-        'session_user' => $jsonSessionUser,
-      ]);
   }
 
+  //FUNCTIONS SOCKET
+
+  //OUVERTURE DE LA PAGE
+  function onOpen(ConnectionInterface $conn) {
+    // Store the new connection to send messages to later
+    $this->currentUser->attach($conn);
+    echo "New connection! ({$conn->resourceId})\n";
+  }
+
+  //FERMETURE DE LA PAGE
+  public function onClose(ConnectionInterface $conn) {
+    // The connection is closed, remove it, as we can no longer send it messages
+    $this->clients->detach($conn);
+
+    echo "Connection {$conn->resourceId} has disconnected\n";
+  }
+
+
+
+
+
 }
+
+
+
+//INSTANCIATION SERIALIZER AVEC CFG
+// $encoders = [new JsonEncoder()];
+// $normalizers = [new ObjectNormalizer()];
+// $serializer = new Serializer($normalizers, $encoders);
+  
+// //SERIALIZE USER OBJECT EN JSON
+// $jsonSessionUser = $serializer->serialize($sessionUser, 'json');
